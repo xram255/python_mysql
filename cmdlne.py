a119b9d002cbcdad972b0114d0083b87c03c715f
RoshanCS <roshan.cs790@gmail.com>
2020-10-30 11:53:23 +0530

    cmdline
    cmdline.py added

--- cmdlne.py
+++ cmdlne.py
@@ -3,20 +3,10 @@ from time import sleep as delay
 
 conn = mycon.connect(host="localhost", user="root", password="", db="test")
 cur = conn.cursor()
-
-
-cur.execute("DROP DATABASE test2")
-delay(1)
-cur.execute("DROP DATABASE test3")
-delay(1)
-
-    
-
-
-cur.execute("SHOW DATABASES")
-for x in cur:
+run_this = input("MySQL Command: ")
+cur.execute(run_this)
+for x  in cur:
     print(x)
-
-
+    
 cur.close()
 conn.close()
