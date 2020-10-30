8a542a532384aae73c07b80af394648362bd2985
RoshanCS <roshan.cs790@gmail.com>
2020-10-30 13:35:58 +0530

    Update corpdb.py
    Creating a table

--- corpdb.py
+++ corpdb.py
@@ -1,3 +1,25 @@
 import mysql.connector as mycon
 
-conn = mycon.connect(host="localhost", user="root", password="")
+conn = mycon.connect(host="localhost", user="root", password="", db="corp")
+
+cur = conn.cursor()
+
+sql = '''CREATE TABLE people
+(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+telephone INT,
+email VARCHAR(255),
+address VARCHAR(100)
+)'''
+
+try:
+    cur.execute(sql)
+except:
+    conn.rollback()
+
+for x in cur:
+    print(x)
+
+cur.close()
+conn.close()
